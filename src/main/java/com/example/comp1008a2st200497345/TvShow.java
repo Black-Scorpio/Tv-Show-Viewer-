a19a9a3b0a4f28e1bc9d37a92b541ff1bab92e59
Scorpio Gentles <ScorpioGentles@outlook.com>
2022-03-30 08:19:34 -0400

    UML diagram added. TvShow class defined

--- src/main/java/com/example/comp1008a2st200497345/TvShow.java
+++ src/main/java/com/example/comp1008a2st200497345/TvShow.java
@@ -1,8 +1,66 @@
 package com.example.comp1008a2st200497345;
 
+import javafx.scene.image.Image;
+
+import java.util.Date;
+
 public class TvShow {
-    private String showTitle, showGenre;
-    private int showRating;
+    private String showTitle;
+    private String showGenre;
+    private int showRating, showSeasons;
+    private Image tvImage;
+    
+    public TvShow(String showTitle, String showGenre, int showRating, int showSeasons, Image tvImage) {
+        setShowTitle(showTitle);
+        setShowGenre(showGenre);
+        setShowRating(showRating);
+        setShowSeasons(showSeasons);
+        setTvImage(tvImage);
+    }
 
 
+
+    public String getShowTitle() {
+        return showTitle;
+    }
+
+    public void setShowTitle(String showTitle) {
+        this.showTitle = showTitle;
+    }
+
+    public String getShowGenre() {
+        return showGenre;
+    }
+
+    public void setShowGenre(String showGenre) {
+        this.showGenre = showGenre;
+    }
+
+    public int getShowRating() {
+        return showRating;
+    }
+
+    public void setShowRating(int showRating) {
+        this.showRating = showRating;
+    }
+
+    public int getShowSeasons() {
+        return showSeasons;
+    }
+
+    public void setShowSeasons(int showSeasons) {
+        this.showSeasons = showSeasons;
+    }
+
+    public Image getTvImage() {
+        return tvImage;
+    }
+
+    public void setTvImage(Image tvImage) {
+        this.tvImage = tvImage;
+    }
+
+    public String toString(){
+        return String.format("Title: %s \n Genre: %s \n Rating: %d \n Seasons: %d", getShowTitle(),getShowGenre(),getShowRating(),getShowSeasons());
+    }
 }
